913ecbebd11034d5fcf296927f28affcfe994def
Iamfitz <vuitsyk.a@gmail.com>
2025-01-07 17:42:23 +0200

    create an instance of CT and OpenAI

--- TwitterAIBot.cs
+++ TwitterAIBot.cs
@@ -15,6 +15,20 @@ namespace Fitz_ai
         private readonly OpenAIAPI _openAIAPI;
         private readonly ILogger<TwitterAIBot> _logger;
         private readonly BotConfiguration _botConfiguration;
+        public TwitterAIBot(BotConfiguration botConfiguration, ILogger<TwitterAIBot> logger)
+        {
+            _botConfiguration = botConfiguration;
+            _logger = logger;
 
+            //Create a TwitterClient instance using data from botConfiguration
+            _twitterClient = new TwitterClient(botConfiguration.Twitter.ConsumerKey,
+                                               botConfiguration.Twitter.ConsumerSecret,
+                                               botConfiguration.Twitter.AccessToken,
+                                               botConfiguration.Twitter.AccessTokenSecret);
+
+            //Create an instance of OpenAIAPI using the API key from botConfiguration.OpenAI
+
+            _openAIAPI = new OpenAIAPI(botConfiguration.OpenAI.ApiKey);
+        }
     }
 }
