bde8ce926ca63751ebc9129ad39f76abf75d5583
Iamfitz <vuitsyk.a@gmail.com>
2025-01-09 13:59:45 +0200

    .cs update

--- Program.cs
+++ Program.cs
@@ -6,13 +6,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Fitz_ai
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            try
+            {
+                // Create configuration from JSON file
+                var configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                // Получаем настройки бота
+                var botConfig = configuration.Get<BotConfiguration>();
+            }
+            catch
+            {
+
+            }
         }
     }
 }
